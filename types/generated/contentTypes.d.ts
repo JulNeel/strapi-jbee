8de85e267bcb90d47beef61947dcbd9f780e6f2e
Julien Bruneel <julien.bruneel@gmail.com>
2026-01-31 23:48:17 +0100

    add ToC boolean into post schema

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -591,6 +591,7 @@ export interface ApiPostPost extends Struct.CollectionTypeSchema {
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     excerpt: Schema.Attribute.Text;
+    GenerateTableOfContent: Schema.Attribute.Boolean;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::post.post'> &
       Schema.Attribute.Private;
